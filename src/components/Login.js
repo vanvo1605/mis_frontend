cf533e65a6cd432b051551f41b39ee32ea2e240f
20231599 <vavo001@ess.ais.ac.nz>
2024-07-30 19:13:43 +1200

    update

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,7 +15,7 @@ function Login(props) {
         setPassword(e.target.value)
     }
 
-    function login () {
+    function lgn () {
         let data = JSON.stringify({
             "username": username,
             "password": password
@@ -49,7 +49,7 @@ function Login(props) {
             <p>Username <input id={"username"} type="text" onChange={usernameHandler}/></p>
             <p>Password <input id={"password"} type="password" onChange={passwordHandler}/></p>
             <p>
-                <button id={"loginbtn"} onClick={login}>Login</button>
+                <button id={"loginbtn"} onClick={lgn}>Login</button>
             </p>
             <p id={'login_status'}>{login_status}</p>
         </div>
